147349c4cc5a109f3a456b3b943cee1972736227
Sydney <sydneyparsonsyb@gmail.com>
2021-07-01 23:12:02 -0400

    map

--- w3d4.rb
+++ w3d4.rb
@@ -18,21 +18,58 @@
 
 # end
 
-class Queue
+# class Queue
+
+#     def initialize
+#         @queue = []
+#     end
+
+#     def push(ele)
+#         @queue.push
+#     end
+
+#     def shift
+#         @queue.shift
+#     end
+
+#     def peak
+#         @queue[0]
+#     end
+# end
+
+class Map
 
     def initialize
-        @queue = []
+        @my_map = []
     end
 
-    def push(ele)
-        @queue.push
+    def set(key, val)
+        if @my_map.empty?
+            @my_map << [key, val]
+        end
+
+        @my_map.each_with_index do |pair, idx|
+            if pair.include?(key) && @my_map[idx][0] == key
+                @my_map[idx][1] = val
+            end
+        end
+
     end
 
-    def shift
-        @queue.shift
+    def get(key)
+        @my_map.each_with_index do |pair, idx|
+            if pair.include?(key) && @my_map[idx][0] == key
+                p key
+            end
+        end
     end
 
-    def peak
-        @queue[0]
+    def delete(key)
+        @my_map.each_with_index do |pair, idx|
+            if pair.include?(key) && @my_map[idx][0] == key
+                @my_map.delete_at(idx)
+            end
+        end
     end
+
 end
