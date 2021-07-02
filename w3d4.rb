f1ae6afac342f22df8fa80f44bae7293919bc226
Sydney <sydneyparsonsyb@gmail.com>
2021-07-01 22:52:11 -0400

    queue

--- w3d4.rb
+++ w3d4.rb
@@ -1,19 +1,38 @@
-class Stack
+# class Stack
+
+#     def initialize
+#         @stack = []
+#     end
+
+#     def push(ele)
+#         @stack << ele
+#     end
+
+#     def pop
+#         @stack.pop
+#     end
+
+#     def peek
+#         @stack[-1]
+#     end
+
+# end
+
+class Queue
 
     def initialize
-        @stack = []
+        @queue = []
     end
 
     def push(ele)
-        @stack << ele
+        @queue.push
     end
 
-    def pop
-        @stack.pop
+    def shift
+        @queue.shift
     end
 
-    def peek
-        @stack[-1]
+    def peak
+        @queue[0]
     end
-
 end
