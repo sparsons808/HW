6a7ea2a768211c3140855258cf0c5b195322c462
Sydney <sydneyparsonsyb@gmail.com>
2021-08-04 23:35:53 -0400

    js

---
+++ callBacks.js
@@ -0,0 +1,10 @@
+// setTimeout((alert) => {
+//     console.log('HAMMERTIME')
+// }, 5000);
+
+// function hammerTime(time) {
+//     window.setTimeout(function () {
+//         console.log(`${time} is hammertime!`);
+//     }, 5000)
+//     console.log(`Hammertime is set for ${time}`);
+// };
